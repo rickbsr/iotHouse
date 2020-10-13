2caa26800a12739dbae1dd2ec641b57d391d6f54
Rick <rick8220602@gmail.com>
2020-10-13 20:59:19 +0800

    1. init commit 2. add UI basic

---
+++ app/src/main/java/com/iot/ihouse/activity/HouseOverviewActivity.java
@@ -0,0 +1,16 @@
+package com.iot.ihouse.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iot.ihouse.R;
+
+public class HouseOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house_overview);
+    }
+}
